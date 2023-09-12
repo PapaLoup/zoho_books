34e6f1dcbf9cc1cc35003c20332c7a2c97782d0c
Raphael <raphael.liere@hotmail.fr>
2023-09-12 15:06:42 +0200

    Refacto auth & invoice base url

--- lib/zoho_books/auth.rb
+++ lib/zoho_books/auth.rb
@@ -2,10 +2,8 @@
 require 'httparty'
 
 class ZohoBooks::Auth
-  BASE_URL = 'https://accounts.zoho.eu/oauth/v2/token'
-
   def self.refresh_access_token
-    response = HTTParty.post("#{BASE_URL}?refresh_token="\
+    response = HTTParty.post("#{ENV['ZOHO_BASE_URL']}/oauth/v2/token?refresh_token="\
                                "#{ENV['ZOHO_REFRESH_TOKEN']}&client_id=#{ENV['ZOHO_CLIENT_ID']}&client_secret="\
                                "#{ENV['ZOHO_CLIENT_SECRET']}&redirect_urrefresh_access_tokeni=#{ENV['ZOHO_REDIRECT_URI']}"\
                                '&grant_type=refresh_token')
